3275baa6d4b8c8076e1d3a9fd1877f527a75c75a
Graham Plata <graham.plata@gmail.com>
2019-03-31 18:14:26 -0400

    Init Particle WorkBench

---
+++ WeatherBuddy/src/WeatherBuddy.ino
@@ -0,0 +1,18 @@
+/*
+ * Project WeatherBuddy
+ * Description: A weather interface for the coffee table.
+ * Author: Graham Plata
+ * Date: March 31, 2019
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
