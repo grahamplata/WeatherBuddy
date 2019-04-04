cac6d77e11b5580a016bda2137667a070876c787
Graham Plata <graham.plata@gmail.com>
2019-04-04 19:08:32 -0400

    working test code

--- WeatherBuddy/src/WeatherBuddy.ino
+++ WeatherBuddy/src/WeatherBuddy.ino
@@ -14,34 +14,51 @@ USARTSerial &nexSerial = Serial1;
 NexText textLow(0, 3, "textLow");
 NexText textHigh(0, 4, "textHigh");
 NexText textTemp(0, 2, "textTemp");
-NexText textLocation(0, 3, "textLocation");
+NexText textLocation(0, 5, "textLocation");
 
 int temperatureLow = 0;
 int temperatureHigh = 0;
 int temperature = 0;
-float humidity = 0;
+
 char buffer[100] = {0};
 
-/*
- * Register a button object to the touch event list.
- */
-
-// setup() runs once, when the device is first turned on.
-void setup(void)
+NexTouch *nex_Listen_List[] = 
 {
-  // Put initialization like pinMode and begin functions here.
+    &textLow,
+    &textHigh,
+    &textTemp,
+    NULL
+};
+
+
+void textTempPopCallback(void *ptr) {
+    NexText *text = (NexText *)ptr;
+    memset(buffer, 0, sizeof(buffer));
+    text->getText(buffer, sizeof(buffer));
+    if (strcmp(buffer,"ON")) {
+        digitalWrite(9, HIGH);
+        strcpy(buffer, "ON");
+    } else {
+        digitalWrite(9, LOW);
+        strcpy(buffer, "OFF");
+    }
+    text->setText(buffer);
+}
+
+// Put initialization like pinMode and begin functions here.
+void setup(void) {
   nexInit();                        // Set the baudrate which is for debug and communicate with Nextion screen.
+  textTemp.attachPop(textTempPopCallback, &textTemp);
   Time.zone(-5);                    // Set Time Zone to EST
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop(void)
-{
+void loop(void) {
   // The core of your code will likely live here.
+  nexLoop(nex_Listen_List);
 }
 
-String getTime()
-{
+String getTime() {
   String timeNow = Time.format(Time.now(), TIME_FORMAT_DEFAULT);
   timeNow = timeNow.substring(11, timeNow.length() - 6);
   return " " + timeNow;
